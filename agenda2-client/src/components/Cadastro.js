45c005687c2cb06ab38afc685d5e15ddbb0194e6
Sidney Sousa <sidney.sousa@ifms.edu.br>
2018-10-18 21:08:21 -0400

    Incrementando a implementação do projeto agenda2-client

--- agenda2-client/src/components/Cadastro.js
+++ agenda2-client/src/components/Cadastro.js
@@ -1,10 +1,69 @@
 import React, { Component } from 'react';
+import { InputText } from 'primereact/inputtext';
+import { InputMask } from 'primereact/inputmask';
 
 class Cadastro extends Component {
 
+    constructor() {
+        super();
+
+        this.state = {
+            nome: '',
+            telefone: ''
+        };
+
+        this.salvar = this.salvar.bind(this);
+        this.aoAlterarValorDoCampo = this.aoAlterarValorDoCampo.bind(this);
+    }
+
+    aoAlterarValorDoCampo(evento) {
+        const nomeCampo = evento.target.name;
+        const valor = evento.target.value;
+        this.setState({ [nomeCampo]: valor });
+    }
+
+    salvar(evento) {
+        evento.preventDefault();
+    }
+
     render() {
         return (
-            <div>Cadastro</div>
+            <form onSubmit={this.salvar}>
+                <br />
+                <div className="p-grid">
+                    <div className="p-col">
+                        <span className="p-float-label">
+                            <InputText
+                                value={this.state.nome}
+                                onChange={this.aoAlterarValorDoCampo}
+                                name="nome"
+                                type="text"
+                                size="30" />
+                            <label htmlFor="nome">Nome</label>
+                        </span>
+                    </div>
+                </div>
+
+                <br />
+
+                <div className="p-grid">
+                    <div className="p-col">
+                        <span className="p-float-label">
+                            <InputMask
+                                mask="(99) 99999-9999"
+                                value={this.state.telefone}
+                                onChange={this.aoAlterarValorDoCampo}
+                                name="telefone"
+                                type="text"
+                                size="30" />
+                            <label htmlFor="telefone">Telefone</label>
+                        </span>
+                    </div>
+                </div>
+
+                <br />
+                <button type="submit" className="botaoSubmit">Salvar</button>
+            </form>
         );
     }
 }
--- agenda2-client/src/components/Consulta.js
+++ agenda2-client/src/components/Consulta.js
@@ -1,6 +1,7 @@
 import React, { Component } from 'react';
 import ContatoService from '../services/ContatoService';
 import { DataTable, Column } from 'primereact/datatable';
+import { Button } from 'primereact/button';
 
 class Consulta extends Component {
 
@@ -21,19 +22,31 @@ class Consulta extends Component {
 
     render() {
         return (
-            <div className="p-grid">
-                <div className="p-md-3"></div>
-                <div className="p-md-6">
-                    <DataTable
-                        value={this.state.contatos}
-                        emptyMessage="Nenhum contato cadastrado"
-                        paginator={true}
-                        rows={5}>
-                        <Column field="nome" header="Nome" sortable={true} />
-                        <Column field="telefone" header="Telefone" />
-                    </DataTable>
+            <div>
+                <div className="p-grid">
+                    <div className="p-col">
+                        <Button
+                            onClick={() => this.props.history.push('cadastro')}
+                            label="Novo contato" />
+                    </div>
                 </div>
-                <div className="p-md-3"></div>
+
+                <br />
+
+                <div className="p-grid">
+                    <div className="p-md-3"></div>
+                    <div className="p-md-6">
+                        <DataTable
+                            value={this.state.contatos}
+                            emptyMessage="Nenhum contato cadastrado"
+                            paginator={true}
+                            rows={5}>
+                            <Column field="nome" header="Nome" sortable={true} />
+                            <Column field="telefone" header="Telefone" />
+                        </DataTable>
+                    </div>
+                    <div className="p-md-3"></div>
+                </div>
             </div>
         );
     }
